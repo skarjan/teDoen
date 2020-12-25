5d92deafdbfe3e8c30e246c720da8c76887bb0c7
Arjan <69347644+skarjan@users.noreply.github.com>
2020-12-25 13:45:29 +0100

    Added lot of functionality

--- js/script.js
+++ js/script.js
@@ -1,18 +1,56 @@
-var x = document.getElementById("pri-knop");
-x.addEventListener("click", addNote);
-// x.addEventListener("click", someOtherFunction);
+// teDoen app
+//
+// 1 -  functions
+//
+//
 
-function getInput(){
-console.log(document.getElementById("invoer").value);
 
+
+//
+//    - addNote function which collects the user input and displays it on the
+//      the page.
+//
+
+// called when button 'add note' is clicked
+function addNote() {
+  return createNote();
 }
 
-function addNote(){
+  function createNote() {
+    // create a blank "p" element
+    let newPara = document.createElement("p");
+      // collect user input to store in newly created element
+      getInput(newPara);
+      // add note-el class for note style
+      addClass(newPara);
+      // returns attached element to parent in the DOM and become visible
+      clearInput();
+        return  appendElement(newPara);
+  }
 
-var newP = document.createElement("p");
-newP.innerHTML = document.getElementById("invoer").value;
-document.getElementById("div").appendChild(newP);
+  function getInput(paraElement) {
+    // collect user input to store in function argument (p element)
+    paraElement.innerHTML = document.getElementById("invoer").value;
+      return paraElement;
+  }
 
-};
+  function appendElement(paraElement) {
+    // attach function argument (p element) to parent element in DOM and becomes
+    // visible
+    return document.getElementById("div").appendChild(paraElement);
+  }
 
-// document.body.appendChild(newNoteElement);
+  function addClass (element) {
+    element.classList.add("note-el", "mt-10");
+    }
+
+  function clearInput() {
+    let inputElement = document.getElementById("invoer");
+    inputElement.value = "";
+  }
+
+console.log(
+  "Hi there!" +
+  "\nThanks for using the teDoen app. \nI hope it helps you."
+  + "\ncreated by A. van der Veen from avdveen.nl"
+);
